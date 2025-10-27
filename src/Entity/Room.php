dc1304c4b312d2134d807f201427891b552a8981
sophiedannery <dannery.sophie@gmail.com>
2025-10-27 13:38:31 +0100

    feature(bdd) : ajout room

---
+++ src/Entity/Room.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RoomRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RoomRepository::class)]
+class Room
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nameRoom = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $noteRoom = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct()
+    {
+        $now = new \DateTimeImmutable('now');
+        $this->createdAt = $now;
+        $this->updatedAt = $now;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameRoom(): ?string
+    {
+        return $this->nameRoom;
+    }
+
+    public function setNameRoom(string $nameRoom): static
+    {
+        $this->nameRoom = $nameRoom;
+
+        return $this;
+    }
+
+    public function getNoteRoom(): ?string
+    {
+        return $this->noteRoom;
+    }
+
+    public function setNoteRoom(?string $noteRoom): static
+    {
+        $this->noteRoom = $noteRoom;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
